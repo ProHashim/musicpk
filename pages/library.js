4205e7559219855b9b19eac20c5a44f39e088651
Prohashim <prohashim4e@gmail.com>
2021-10-29 21:18:50 +0500

    created repo

---
+++ pages/library.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/dist/shared/lib/head";
+
+const library = () => {
+    return (
+        <div>
+            <head>
+                <title>
+                    MusicPk Library
+                </title>
+                <meta name="keywords" content="music, pakistan" />
+            </head>
+            <h1>Library here: </h1>
+        </div >
+    )
+}
+
+export default library
